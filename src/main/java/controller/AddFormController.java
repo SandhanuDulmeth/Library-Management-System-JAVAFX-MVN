1d6f7dc411dde4b6ec0c0ad35e546d2126555f16
SandhanuDulmeth <sandhanu12@gmail.com>
2024-12-27 22:55:49 +0530

    Commit

--- src/main/java/controller/AddFormController.java
+++ src/main/java/controller/AddFormController.java
@@ -34,10 +34,12 @@ public class AddFormController implements Initializable {
     public JFXTextField txtpublishedYear1;
     public JFXTextField txtGenre1;
     public JFXTextField txtPrice1;
-    public JFXTextField txtId11;
-    public JFXTextField txtName11;
-    public JFXTextField txtAddress11;
-    public JFXTextField txtSalary11;
+    public JFXTextField txtId2;
+    public JFXTextField txtTitle2;
+    public JFXTextField txtAuthor2;
+    public JFXTextField txtpublishedYear2;
+    public JFXTextField txtGenre2;
+    public JFXTextField txtPrice2;
 
 
 
@@ -176,28 +178,30 @@ public class AddFormController implements Initializable {
         loadTable();
     }
 
-//    public void SearchOnKeyReleased(KeyEvent keyEvent) {
-//        Connection connection = null;
-//        try {
-//            connection = DBConnection.getInstance().getConnection();
-//
-//            PreparedStatement stm = connection.prepareStatement("SELECT * FROM book WHERE BookID = ?");
-//            stm.setObject(1, txtId11.getText());
-//            ResultSet resultSet = stm.executeQuery();
-//            if (resultSet.next()) {
-//
-//                txtName11.setText(resultSet.getString(2));
-//                txtAddress11.setText(resultSet.getString(3));
-//                txtSalary11.setText(resultSet.getString(4));
-//
-//            }
-//
-//        } catch (ClassNotFoundException | SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//
-//    }
+    public void SearchOnKeyReleased(KeyEvent keyEvent) {
+        Connection connection = null;
+        try {
+            connection = DBConnection.getInstance().getConnection();
+
+            PreparedStatement stm = connection.prepareStatement("SELECT * FROM book WHERE BookID = ?");
+            stm.setObject(1, txtId2.getText());
+            ResultSet resultSet = stm.executeQuery();
+            if (resultSet.next()) {
+
+                txtTitle2.setText(resultSet.getString(2));
+                txtAuthor2.setText(resultSet.getString(3));
+                txtpublishedYear2.setText(resultSet.getString(4));
+                txtGenre2.setText(resultSet.getString(5));
+                txtPrice2.setText(resultSet.getString(6));
+
+            }
+
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+    }
 
 
     public void btnUpdateOnAction(ActionEvent actionEvent) {
@@ -205,11 +209,13 @@ public class AddFormController implements Initializable {
         try {
             connection = DBConnection.getInstance().getConnection();
 
-            PreparedStatement stm = connection.prepareStatement("UPDATE book SET name = ?, address = ?, salary = ? WHERE BookID = ?");
-            stm.setObject(4, txtId11.getText());
-            stm.setObject(1, txtName11.getText());
-            stm.setObject(2, txtAddress11.getText());
-            stm.setObject(3, txtSalary11.getText());
+            PreparedStatement stm = connection.prepareStatement("UPDATE book SET Title = ?, Author = ?, PublishedYear = ?, Genre = ?, Price = ? WHERE BookID = ?");
+            stm.setObject(6, txtId2.getText());
+            stm.setObject(1, txtTitle2.getText());
+            stm.setObject(2, txtAuthor2.getText());
+            stm.setObject(3, txtpublishedYear2.getText());
+            stm.setObject(4, txtGenre2.getText());
+            stm.setObject(5, txtPrice2.getText());
             int i = stm.executeUpdate();
             if (i > 0) {
                 new Alert(Alert.AlertType.INFORMATION, "Updated " + txtId.getText()).show();
@@ -223,10 +229,12 @@ public class AddFormController implements Initializable {
         }
 
 
-        txtId11.setText(null);
-        txtName11.setText(null);
-        txtAddress11.setText(null);
-        txtSalary11.setText(null);
+        txtId2.setText(null);
+        txtTitle2.setText(null);
+        txtAuthor2.setText(null);
+        txtpublishedYear2.setText(null);
+        txtGenre2.setText(null);
+        txtPrice2.setText(null);
         loadTable();
     }
 
